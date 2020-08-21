c12a4aeb516c3e3df089486f4f2b4e0d72b1f133
Carlos Sosa <cesosag@gmail.com>
2020-08-21 17:45:32 -0400

    Adjust Videos section

--- components/Videos/index.jsx
+++ components/Videos/index.jsx
@@ -1,22 +1,24 @@
 import PropTypes from 'prop-types'
 import { YTVideo as Video, Button } from 'components'
 import { texts } from 'services'
-import { Wrapper } from './videos.styles'
+import { Wrapper, Title, CTAs } from './videos.styles'
 
 const Videos = ({ videos }) => (
 	<>
-		<h3>Ver sermones anteriores</h3>
+		<Title>Ver sermones anteriores</Title>
 		<Wrapper>
 			{videos.map(({ snippet: { resourceId: { videoId } } }) => <Video key={videoId} videoID={videoId} />)}
 		</Wrapper>
-		<Button
-			href={`https://www.youtube.com/channel/${videos[0]?.snippet?.channelId}/`}
-			target="_blank"
-			rel="noreferrer"
-			size="small"
-		>
-			{texts.BUTTONS.VIEW_MORE}
-		</Button>
+		<CTAs>
+			<Button
+				href={`https://www.youtube.com/channel/${videos[0]?.snippet?.channelId}/`}
+				target="_blank"
+				rel="noreferrer"
+				size="small"
+			>
+				{texts.BUTTONS.VIEW_MORE}
+			</Button>
+		</CTAs>
 	</>
 )
 
--- components/Videos/videos.styles.js
+++ components/Videos/videos.styles.js
@@ -1,7 +1,22 @@
 import styled from 'styled-components'
+import { theme } from 'styles'
+
+const { colors, typography } = theme
 
 export const Wrapper = styled.div`
 	display: grid;
 	grid-gap: 1em;
 	grid-template-columns: repeat(auto-fit, minmax(320px, 1fr))
+`
+
+export const Title = styled.h3`
+	color: ${colors.blue.light};
+	font-family: ${typography.primaryFont};
+`
+
+export const CTAs = styled.div`
+	display: flex;
+	justify-content: space-evenly;
+	margin-top: 1.5em;
+	width: 100%;
 `
