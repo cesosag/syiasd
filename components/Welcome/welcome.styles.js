5a0f3419891df925d2e8e230dd7d4e39f4cd2b31
Carlos Sosa <cesosag@gmail.com>
2020-09-21 20:21:37 -0400

    Fix gradient for Hero

--- components/Welcome/welcome.styles.js
+++ components/Welcome/welcome.styles.js
@@ -4,12 +4,12 @@ import { theme } from 'styles'
 const { colors, gradients, mediaQueries: { phoneOnly } } = theme
 
 export const Section = styled.section`
-	${({ background }) => (background ? `background-image: ${gradients.violet}, url(${background});` : null)}
+	${({ background }) => (background ? `background-image: url('/images/hero-gradient.svg'), url(${background});` : null)}
 	background-color: ${colors.violet};
 	background-repeat: no-repeat;
-	background-size: 100% 45%, contain;
+	background-size: contain;
 
 	${phoneOnly} {
-		background-size: 100% 45%, 200% auto;
+		background-size: 200% auto;
 	}
 `
