6c0b73dc7dd7ddd141a35d2d28ba0575bb08d52f
Carlos Sosa <cesosag@gmail.com>
2020-09-23 18:02:58 -0400

    Add favicon

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -29,6 +29,15 @@ export default class Document extends NextDocument {
 		return (
 			<Html lang="es">
 				<Head>
+					<link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+					<link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+					<link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+					<link rel="manifest" href="/favicon/site.webmanifest" />
+					<link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#1b0939" />
+					<link rel="shortcut icon" href="/favicon/favicon.ico" />
+					<meta name="msapplication-TileColor" content="#1b0939" />
+					<meta name="msapplication-config" content="/favicon/browserconfig.xml" />
+					<meta name="theme-color" content="#1b0939" />
 					<link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet" />
 				</Head>
 				<body>
